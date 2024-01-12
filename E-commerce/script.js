2b96757d0e5a1ae7a48750f4091d9278ae0a9eec
Afaq Ali <133626948+Afaq-ali1@users.noreply.github.com>
2024-01-12 12:22:16 +0500

    Update script.js

--- E-commerce/script.js
+++ E-commerce/script.js
@@ -1,1 +1,12 @@
-document.getElementById("").style.color="pink";
+const cardBtnElements = document.getElementsByClassName('cardBtn');
+function handleCardBtnClick() {
+    // Add your click event handling logic here
+    console.log("Card button clicked!");
+}
+cardBtnElements.addEventListener('click', handleCardBtnClick);
+
+// document.getElementById("#container2").style.backgroundColor='blue';
+console.log('This is data');
+console.error('this is error');
+console.info('this is info');
+console.warn('this is warn');
